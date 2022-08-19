cf2f4f5a50f13d8c8bb02d9f515f2dc31b8cb343
Paulo Carvalho <110569663+PauloCG2001@users.noreply.github.com>
2022-08-18 22:30:06 -0300

    Update login.js

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function iniciar(){
+    let email = document.getElementById("email").value;
+    let contra = document.getElementById("contra").value;
+    
+    if(email === "" || contra === ""){
+       alert ("Debe llenar todos los campos")
+       }
+       else {location.href = "index.html";}
+
+    }
+
+    let boton = document.getElementById("boton");
+
+    boton.addEventListener("click", function(){
+       iniciar()
+    })
